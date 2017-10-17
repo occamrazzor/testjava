cf592dea04153b2b437276d55d13df145721374b
occamrazzor <occam_razor@hotmail.com>
2017-10-17 06:48:10 -0700

    Added Iteration

--- Git_ProjectJava/src/occam/java/MainJava.java
+++ Git_ProjectJava/src/occam/java/MainJava.java
@@ -3,8 +3,23 @@ package occam.java;
 public class MainJava {
 
 	public static void main(String[] args) {
-		System.out.println("This is a new line");
-
+		
+		int result = 0;
+		int a []  = {2, 5, 4, 8, 7};
+		
+		for (int i = 0; i < a.length; i++) {
+			result += a[i];
+			
+			if (a[i] == 7){
+				System.out.println(a[i]);
+				break;
+				
+			}
+		
+		}
+		System.out.println("The result of the sum of values in the array is: " + result);
+		
+		
 	}
 
 }
